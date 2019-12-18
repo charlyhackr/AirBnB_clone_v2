f234350f3d525e77de2e9d32a97d8ca0da398273
charlyhackr <930@holbertonschool.com>
2019-12-17 19:58:19 -0500

    setu mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,18 @@
+-- Creates a MYsql server with:
+--   Database hbnb_dev_db
+--   GRANTS all privileges  for hbnb_dev
+--   Grants SELECT  privilege for hbnb_dev
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
