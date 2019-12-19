32c6b433e03f91a20d68133ebec987db4c58f9b2
charlyhackr <930@holbertonschool.com>
2019-12-18 21:57:58 -0500

    add places and base model

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ FileStorage relationship between State and City. """
+            ret = []
+            for key, value in models.storage.all(City).items():
+                if value.state_id == self.id:
+                    ret.append(value)
+            return ret
